c221ae8866adad0943c2c384948007a256e30d62
Dvwhite81 <Dvwhite1981@gmail.com>
2024-01-10 09:39:54 -0500

    complete

--- classes/student.js
+++ classes/student.js
@@ -1,7 +1,22 @@
 const Person = require('./person');
 
 // Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
 
+  static compareGPA(student1, student2) {
+    const gpa1 = student1.GPA;
+    const gpa2 = student2.GPA;
+
+    if (gpa1 === gpa2) return 'Both students have the same GPA';
+    const higher = gpa1 > gpa2 ? student1 : student2;
+    return `${higher.firstName} ${higher.lastName} has the higher GPA.`;
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
